693086398ab50e0c25f0c7268a57fe57eac6e95f
ratnakumar ramanan <ratram2010@yahoo.co.uk>
2017-11-09 23:08:02 +0000

    updated calender

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,7 +4,7 @@ import React from "react";
 function Input(props){
     return(
         <div class="col">
-            <input type="text" class="form-control" placeholder="Enter Your Activation code" />
+            <input type="text" class="form-control" placeholder="Enter Your Activation code"  onChange={this.handleInput} />
         </div>
     )}
 
