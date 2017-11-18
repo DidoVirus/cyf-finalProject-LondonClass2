f480f7315d866c8cd1e79fe369b2c44853f80bf0
ratnakumar ramanan <ratram2010@yahoo.co.uk>
2017-11-18 14:10:47 +0000

    'package.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import Header from './components/Header';
 import Home from './container/Home.jsx';
 import Activation from './container/Activation.jsx';
 import Dashboard from './container/Dashboard.jsx';
 import Meeting from './container/Meeting.js';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <Meeting />
-    );
-  }
-}
+
+
 
 export default App;
